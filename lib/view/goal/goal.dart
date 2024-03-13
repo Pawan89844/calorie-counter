4e7d0941ed9ac29c65bc9275320bd6566bf8ced6
Pawan Kumar <pk89844@gmail.com>
2024-03-13 18:39:48 +0530

    CU-860t7wz1z - Create Grid with options

--- lib/view/goal/goal.dart
+++ lib/view/goal/goal.dart
@@ -1,5 +1,7 @@
+import 'package:caloriescount/view/goal/model/goal_model.dart';
 import 'package:caloriescount/view/widget/AppHeadingText.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import '../widget/AppText.dart';
 
@@ -32,6 +34,36 @@ class _GoalState extends State<Goal> {
                 ],
               ),
             ),
+            GridView.builder(
+              shrinkWrap: true,
+              primary: false,
+              scrollDirection: Axis.vertical,
+              itemCount: GoalModel.goals.length,
+              physics: const NeverScrollableScrollPhysics(),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 6 / 5,
+                  crossAxisSpacing: 10.0,
+                  mainAxisSpacing: 10.0),
+              itemBuilder: (context, index) {
+                final GoalModel items = GoalModel.goals[index];
+                return Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black12),
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(12.0))),
+                  alignment: Alignment.center,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      SvgPicture.asset(items.image),
+                      AppText(items.title),
+                    ],
+                  ),
+                );
+              },
+            )
           ],
         ),
       )),
---
+++ lib/view/goal/model/goal_model.dart
@@ -0,0 +1,24 @@
+class GoalModel {
+  final int id;
+  final String title;
+  final String image;
+
+  GoalModel({required this.id, required this.title, required this.image});
+  static const String _baseAsset = 'assets/';
+
+  // String _baseAsset() {
+  //   return 'assets/';
+  // }
+
+  static List<GoalModel> goals = <GoalModel>[
+    GoalModel(id: 1, title: 'Lose Weight', image: '${_baseAsset}surface1.svg'),
+    GoalModel(id: 2, title: 'Build Muscle', image: '${_baseAsset}bicep.svg'),
+    GoalModel(
+        id: 3,
+        title: 'Increase Energy',
+        image: '${_baseAsset}lightning-bolt.svg'),
+    GoalModel(id: 4, title: 'Improve Sleep', image: '${_baseAsset}sleep.svg'),
+    GoalModel(id: 5, title: 'Manage Glucose', image: '${_baseAsset}blood.svg'),
+    GoalModel(id: 6, title: 'Be Healthier', image: '${_baseAsset}flat.svg'),
+  ];
+}
