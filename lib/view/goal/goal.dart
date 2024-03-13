ca3b87c6cf5d9adbbb032ed8409eea0446f042b5
Pawan Kumar <pk89844@gmail.com>
2024-03-13 17:38:26 +0530

    CU-860t7wz1v - Add Heading

--- lib/view/goal/goal.dart
+++ lib/view/goal/goal.dart
@@ -1,3 +1,4 @@
+import 'package:caloriescount/view/widget/AppHeadingText.dart';
 import 'package:flutter/material.dart';
 
 class Goal extends StatefulWidget {
@@ -10,6 +11,21 @@ class Goal extends StatefulWidget {
 class _GoalState extends State<Goal> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 14.0),
+        child: Column(
+          children: [
+            Container(
+              alignment: Alignment.centerLeft,
+              height: 120.0,
+              width: double.infinity,
+              child: const AppHeadingText('What are your goals?'),
+            ),
+          ],
+        ),
+      )),
+    );
   }
 }
