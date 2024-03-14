6262bc6eee87e8d3d0069b5d7804b7bcbb297f07
Pawan Kumar <pk89844@gmail.com>
2024-03-14 19:06:38 +0530

    CU-860t7wz61 - Add Drop Down

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:caloriescount/view/goal/goal_birthday.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const GoalGender(),
+      home: GoalBirthday(),
     );
   }
 }
