7e145c09f2c52a982b31b8032ace941b4fb17fd6
Pawan Kumar <pk89844@gmail.com>
2024-03-19 13:09:07 +0530

    Goal Controller Documentation

--- lib/view/goal/controller/goal_controller.dart
+++ lib/view/goal/controller/goal_controller.dart
@@ -5,15 +5,19 @@ import '../logic/goal_info.dart';
 enum GoalPage { latestWeight, goalWeight, gender, birthday, currentPage, home }
 
 class GoalController extends GetxController {
+  ///Goal Info is a Logic class which contains static data model creation part.
   final GoalInfo _goal = GoalInfo();
-
   final RxList _selectedGoal = [].obs;
 
+  /// [_selectedGoal] List keeps track of selected Goals category from [GoalsGrid].
+  /// private value can't be modified directly.
   RxList get currentGoal => _selectedGoal;
   final Rx<GoalPage> _currenPage = Rx<GoalPage>(GoalPage.currentPage);
 
+  /// current page property is the [GoalPage] index.
   Rx<GoalPage> get currentPage => _currenPage;
 
+  /// Set Goal Method is for adding the Goal into [_selectedGoal] List
   void setGoal(int itemId) {
     if (!_selectedGoal.contains(itemId)) {
       _selectedGoal.add(itemId);
@@ -22,12 +26,14 @@ class GoalController extends GetxController {
     }
   }
 
+  /// A Private method which reduces the redundency of assigning the page, heading, and subheading.
   void _setPage(GoalPage page) {
     _currenPage.value = page;
     _goal.heading(_currenPage.value);
     _goal.subHeading(_currenPage.value);
   }
 
+  /// setGoal method is for setting the page based on the [_currentPage.value]. It sets the next page from current page.
   void setGoalPage() {
     switch (_currenPage.value) {
       case GoalPage.goalWeight:
